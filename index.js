7450d7b7bcdad8930d7a18f93e4d16348fa4acef
NiceDayZ <craciunmihai40@gmail.com>
2020-06-07 13:00:46 +0300

    cors is a little ..

--- index.js
+++ index.js
@@ -48,6 +48,10 @@ const router = new Router();
 
 //router.use(cors({credentials: true, origin: "*", allowedHeaders : ['x-auth-token', 'Content-Type', 'Access-Control-Allow-Headers', 'Authorization', 'X-Requested-With'], exposedHeaders : ['x-auth-token','Content-Type', 'Access-Control-Allow-Headers', 'Authorization', 'X-Requested-With'], methods : "GET,PUT,PATCH,POST,DELETE,OPTIONS"}));
 
+ router.use(function(req, res, next){
+  console.log(req.headers);
+ });
+
  router.use(function(req, res, next) {
    res.setHeader("Access-Control-Allow-Origin", "*");
    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
