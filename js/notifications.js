f7cf95ea629ecff39a41df5346af8d5c60b22645
NiceDayZ <craciunmihai40@gmail.com>
2020-06-11 20:34:08 +0300

    hotfix

--- js/notifications.js
+++ js/notifications.js
@@ -7,7 +7,9 @@ function notifyMe() {
     // Let's check whether notification permissions have already been granted
     else if (Notification.permission === "granted") {
       // If it's okay let's create a notification
-      var notification = new Notification("Hi there!");
+      var img = '../images/editabil-umbrela.png';
+      var text = 'Welcome to UmbrElla';
+      var notification = new Notification('UmbrElla', { body: text, icon: img });
     }
   
     // Otherwise, we need to ask the user for permission
@@ -15,11 +17,15 @@ function notifyMe() {
       Notification.requestPermission().then(function (permission) {
         // If the user accepts, let's create a notification
         if (permission === "granted") {
-          var notification = new Notification("Hi there!");
+            var img = '../images/editabil-umbrela.png';
+            var text = 'Welcome to UmbrElla';
+            var notification = new Notification('UmbrElla', { body: text, icon: img });
         }
       });
     }
   
     // At last, if the user has denied notifications, and you 
     // want to be respectful there is no need to bother them any more.
-  }
+  };
+
+  notifyMe();
